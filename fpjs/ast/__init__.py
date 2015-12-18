5d84b931fb9f13ad1a40503f2edc7c326dc6413f
rapidhere <rapidhere@gmail.com>
2015-12-18 14:07:15 +0800

    framework job

---
+++ fpjs/ast/__init__.py
@@ -0,0 +1,24 @@
+# -*-codiing: utf-8 -*-
+"""
+Copyright (c) 2015 by rapidhere, RANTTU. INC. All Rights Reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the Lesser GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+predefined licenses headers
+ref from http://opensource.org
+
+parse javascript code into AST(abstract syntax tree)
+"""
+
+__author__ = "rapidhere"
