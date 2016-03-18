1466ff20d3253c64b78a3f7bf07398a2816307b2
rapidhere <rapidhere@gmail.com>
2016-03-18 16:23:07 +0800

    add complex while while test case

---
+++ test/conv/comp-while-while.js
@@ -0,0 +1,28 @@
+var sum = 0, i = 0, j;
+
+while(1) {
+  j = 0;
+
+  if(i >= 12)
+    break;
+
+  if(i % 3 == 0) {
+    i += 1;
+    continue;
+  }
+
+  while(1) {
+    j += 1;
+
+    if(j >= i + 1) {
+      break;
+    }
+
+    if((i + j) % 4 == 0)
+      continue;
+
+    sum += i + j;
+  }
+
+  i += 1;
+}
