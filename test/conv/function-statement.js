2224cb39869c0a2f0a54db91ddf51d8907db3c1d
The Goddamn Awesome Godfather Ever RAPID <rapidhere@gmail.com>
2016-05-23 17:23:43 +0800

    enhancement the function statement testcase

--- test/conv/function-statement.js
+++ test/conv/function-statement.js
@@ -1,4 +1,5 @@
 console.log(func());
+console.log(func1());
 
 function func() {
     return 1 + 1;
@@ -7,3 +8,11 @@ function func() {
 var func = function() {
 
 };
+
+function func1() {
+    return func() + func2();
+
+    function func2() {
+        return 2;
+    }
+}
